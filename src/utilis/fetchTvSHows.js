7a658b2545cb80567e0f9d9835cb870d792c3e92
sarah-svg <kjskillen@gmail.com>
2021-08-13 14:12:33 -0700

    added a s

--- src/utilis/fetchTvSHows.js
+++ src/utilis/fetchTvSHows.js
@@ -1,6 +1,6 @@
 export const fetchTvShows = () => {
   // eslint-disable-next-line max-len
-  return fetch('http://api.tvmaze.com/shows?page=1').then((res) => res.json());
+  return fetch('https://api.tvmaze.com/shows?page=1').then((res) => res.json());
 
 };
 
