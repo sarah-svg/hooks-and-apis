1e9d1661b0f28c31ca34e7e690965843d23e29a7
sarah-svg <kjskillen@gmail.com>
2021-08-13 14:03:12 -0700

    add a link to all components in the header

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import JokesContainer from '../../container/JokesContainoer';
 import TvContainer from '../../container/TvContainer';
 import Characters from '../../container/Characters';
@@ -13,6 +13,27 @@ import RickAndMortyContaionor from '../../container/RickAndMortyContainor';
 export default function App(){
   return  (
     <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/tv">Tv shows</Link>
+            </li>
+            <li>
+              <Link to="/joke">Random Jokes</Link>
+            </li>
+            <li>
+              <Link to="hey">Hey Arnold Characters</Link>
+            </li>
+            <li>
+              <Link to="/rick">Rick and Morty characters</Link>
+            </li>
+            <li>
+              <Link to="/twin">Twin peeks quotes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
       <Switch>
         <Route exact path="/tv" component={TvContainer}/>
         <Route exact path="/joke" component={JokesContainer}/>
